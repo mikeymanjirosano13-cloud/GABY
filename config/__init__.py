650d64d1374ba3d4e92a88034ac292601d30573f
Sun4kko <mikeymanjirosano13@gmail.com>
2026-07-24 21:20:36 -0300

    Create __init__.py with package documentation
    Add initial documentation for the config package

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pacote config.
+
+Contém as configurações centrais do projeto G.A.B.Y.
+Separar configuração de lógica de negócio é um princípio
+de Clean Architecture: o "core" nunca deve depender de
+detalhes de infraestrutura ou de configuração espalhada.
+"""
